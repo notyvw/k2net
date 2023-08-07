d2b04d9d7325a6b68bc899845ef2f628007f978a
umaryoyono <umaryoyono@gmail.com>
2023-08-07 19:00:57 +0700

    anyar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -126,54 +126,87 @@
 
     {{-- Service --}}
     <section id="service">
-        <div class="grid grid-cols-12 gap-6 mx-40 my-24">
-            <div class="col-span-12 text-center">
-                <p class="font-semibold text-2xl">Our Service</p>
-                <p class="font-medium">Layanan yang kita tawarkan untuk anda</p>
+        <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+            <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                <svg id="service" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg>
             </div>
-
-            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
-                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
-                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
-                </div>
-                <p class="text-xl font-bold my-2.5 tracking-wide">Design</p>
-                <p class="font-medium mx-10 mb-10">Membuat tampilan instagram anda lebih menarik</p>
-            </div>
-            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
-                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
-                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
-                </div>
-                <p class="text-xl font-bold my-2.5 tracking-wide">Photo</p>
-                <p class="font-medium mx-10 mb-10">Membuat foto produk anda lebih menarik</p>
-            </div>
-            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
-                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
-                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg>
-                </div>
-                <p class="text-xl font-bold my-2.5 tracking-wide">Video</p>
-                <p class="font-medium mx-10 mb-10">Membuat konsep, pengambilan video dan mengedit video</p>
-            </div>
-            <div class="col-span-2 h-20 ">
-
-            </div>
-            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
-                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
-                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
-                </div>
-                <p class="text-xl font-bold my-2.5 tracking-wide">Caption</p>
-                <p class="font-medium mx-10 mb-10">Membuat bahasa yang persuasif untuk menarik audiens</p>
-            </div>
-            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
-                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
-                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64H240l-10.7 32H160c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H346.7L336 416H512c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zM512 64V352H64V64H512z"/></svg>
-                </div>
-                <p class="text-xl font-bold my-2.5 tracking-wide">Landing Page</p>
-                <p class="font-medium mx-10 mb-10">Membuat halaman website untuk kepentingan marketing</p>
-            </div>
-            <div class="col-span-2 h-20 ">
-
-            </div>
+            <p class="text-xl font-bold my-2.5 tracking-wide">Video</p>
+            <p class="font-medium mx-10 mb-10">Membuat konsep, pengambilan video dan mengedit video</p>
         </div>
+
+                <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+                <style>
+                  .slide {
+                    opacity: 0;
+                    transition: opacity 0.5s ease-in-out;
+                  }
+
+                  .active {
+                    opacity: 1;
+                  }
+                </style>
+              </head>
+
+              <body class="bg-gray-100">
+                <div class="container mx-auto px-4 py-8">
+                  <h1 class="text-2xl font-semibold text-gray-800 mb-4">Layanan Kami</h1>
+                  <div class="relative">
+                    <!-- Slide 1 -->
+                    <div class="slide active">
+                      <div class="bg-white shadow-lg rounded-lg p-4">
+                        <h2 class="text-lg font-semibold text-gray-800">Layanan 1</h2>
+                        <p class="text-gray-600 mt-2">Deskripsi singkat tentang Layanan 1.</p>
+                      </div>
+                    </div>
+
+                    <!-- Slide 2 -->
+                    <div class="slide">
+                      <div class="bg-white shadow-lg rounded-lg p-4">
+                        <h2 class="text-lg font-semibold text-gray-800">Layanan 2</h2>
+                        <p class="text-gray-600 mt-2">Deskripsi singkat tentang Layanan 2.</p>
+                      </div>
+                    </div>
+
+                    <!-- Slide 3 -->
+                    <div class="slide">
+                      <div class="bg-white shadow-lg rounded-lg p-4">
+                        <h2 class="text-lg font-semibold text-gray-800">Layanan 3</h2>
+                        <p class="text-gray-600 mt-2">Deskripsi singkat tentang Layanan 3.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="flex justify-center mt-4">
+                    <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md mr-2" id="prevBtn">Sebelumnya</button>
+                    <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md" id="nextBtn">Selanjutnya</button>
+                  </div>
+                </div>
+
+                <script>
+                  const slides = document.querySelectorAll('.slide');
+                  const prevBtn = document.getElementById('prevBtn');
+                  const nextBtn = document.getElementById('nextBtn');
+                  let currentSlide = 0;
+
+                  function showSlide(n) {
+                    slides.forEach(slide => slide.classList.remove('active'));
+                    slides[n].classList.add('active');
+                  }
+
+                  prevBtn.addEventListener('click', () => {
+                    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+                    showSlide(currentSlide);
+                  });
+
+                  nextBtn.addEventListener('click', () => {
+                    currentSlide = (currentSlide + 1) % slides.length;
+                    showSlide(currentSlide);
+                  });
+
+                  // Show initial slide
+                  showSlide(currentSlide);
+                </script>
+              </body>
     </section>
 
     {{-- Contacts --}}
---
+++ resources/views/jasa.blade.php
@@ -0,0 +1,42 @@
+<div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
+                </div>
+                <p class="text-xl font-bold my-2.5 tracking-wide">Design</p>
+                <p class="font-medium mx-10 mb-10">Membuat tampilan instagram anda lebih menarik</p>
+            </div>
+            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
+                </div>
+                <p class="text-xl font-bold my-2.5 tracking-wide">Photo</p>
+                <p class="font-medium mx-10 mb-10">Membuat foto produk anda lebih menarik</p>
+            </div>
+            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg>
+                </div>
+                <p class="text-xl font-bold my-2.5 tracking-wide">Video</p>
+                <p class="font-medium mx-10 mb-10">Membuat konsep, pengambilan video dan mengedit video</p>
+            </div>
+            <div class="col-span-2 h-20 ">
+
+            </div>
+            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
+                </div>
+                <p class="text-xl font-bold my-2.5 tracking-wide">Caption</p>
+                <p class="font-medium mx-10 mb-10">Membuat bahasa yang persuasif untuk menarik audiens</p>
+            </div>
+            <div class="col-span-4 text-center rounded-xl bg-white shadow-xl shadow-gray-400">
+                <div class="p-2 flex justify-center items-center rounded-full mx-auto bg-blue-500 w-14 h-14 mt-10">
+                    <svg id="service" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64H240l-10.7 32H160c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H346.7L336 416H512c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zM512 64V352H64V64H512z"/></svg>
+                </div>
+                <p class="text-xl font-bold my-2.5 tracking-wide">Landing Page</p>
+                <p class="font-medium mx-10 mb-10">Membuat halaman website untuk kepentingan marketing</p>
+            </div>
+            <div class="col-span-2 h-20 ">
+
+            </div>
+        </div>
