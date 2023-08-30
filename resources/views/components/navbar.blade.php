b74826048b4530bd174b95e66e3dafa111f20980
umaryoyono <umaryoyono@gmail.com>
2023-08-30 22:33:01 +0700

    nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,20 +5,17 @@
           {{-- <img src="Cyber Security.png" /> --}}
           <span class="self-center sm:text-2xl font-semibold whitespace-nowrap text-white">K2 NET</span>
       </a>
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
-            <li id="menu-item-46988" class="new-mega-menu menu-col-3 nav item">
-                <a href="#product" data-scroll class="block py-2 pl-3 pr-4 text-white transform transition-transform hover:scale-110 rounded md:bg-transparent md:p-0 aria-current="page">Product</a>
-            </li>
-            <li>
-              <a href="#service" data-scroll class="block py-2 pl-3 pr-4 text-white transform transition-transform hover:scale-110 rounded  md:border-0 md:p-0">Services</a>
-            </li>
             <li>
                 <a href="#pricing" data-scroll class="block py-2 pl-3 pr-4 text-white transform transition-transform hover:scale-110 rounded  md:border-0 md:p-0">Pricing</a>
+            </li>
+            <li id="menu-item-46988" class="new-mega-menu menu-col-3 nav item">
+                <a href="#product" data-scroll class="block py-2 pl-3 pr-4 text-white transform transition-transform hover:scale-110 rounded md:bg-transparent md:p-0 aria-current="page">Product</a>
             </li>
             <li>
                 <a href="#contact" data-scroll class="block py-2 pl-3 pr-4 text-white transform transition-transform hover:scale-110 rounded  md:border-0 md:p-0">Contact</a>
